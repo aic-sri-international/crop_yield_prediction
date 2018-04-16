b5b06dfcafd31af531246d62d8677628d0d2e2d9
Clint Cooper <clint.cooper@sri.com>
2018-04-16 15:52:34 -0600

    Adjusted spacing and formatting

--- 2 clean data/final_save_histogram.py
+++ 2 clean data/final_save_histogram.py
@@ -1,18 +1,10 @@
-import numpy as np
-import scipy.io as io
+import csv
 import math
 import os
 from os import listdir
-from os.path import basename, isfile, join
-import skimage.io
-import pandas as pd
-# from clean_data_county import *
-from joblib import Parallel, delayed
-import multiprocessing
-import final_clean_data as clean
-import shutil
-import csv
 
+import numpy as np
+from os.path import basename, isfile, join
 
 """histogram creation module for crop_yield_prediction
 
@@ -33,13 +25,15 @@ Run this module:
 
 """
 
+
 class CropHistograms():
-    """Read in numpy data and convert to histograms
+    """
+    Read in numpy data and convert to histograms.
 
     Parameters
     ----------
     output_dir : str
-        file path of directory with files of name <year>_<state_num>_<county_num>.npy, containaing the numpy arrays
+        file path of directory with files of name <year>_<state_num>_<county_num>.npy, containing the numpy arrays
         outputted by final_clean_data
         Also the directory where histograms will be saved
     data_yield_file : str
@@ -49,15 +43,16 @@ class CropHistograms():
     locations_file : str
         file path that contains the latitude and longitude values corresponding to the proper output_dir file
 
-        """
+    """
+
     def __init__(self, output_dir, data_yield_file, data_yield_subset_file, locations_file):
         """
+
         Parameters
         ----------
         output_dir : str
-            file path of directory with files of name <year>_<state_num>_<county_num>.npy, containaing the numpy arrays
-            outputted by final_clean_data
-            Also the directory where histograms will be saved
+            file path of directory with files of name <year>_<state_num>_<county_num>.npy, containing the numpy arrays
+            outputted by final_clean_data and the directory where histograms will be saved
         data_yield_file : str
             file path containing the csv info corresponding to the data yield for each location and year
         data_yield_subset_file : str
@@ -72,28 +67,29 @@ class CropHistograms():
         # fill in subset yield
         self.create_yield_csv()
         self.data_yield = np.genfromtxt(data_yield_subset_file, delimiter=',')
-                
+
         # generate index for all data
         length = self.data_yield.shape[0]
         self.index_all = np.arange(length)
 
         self.year = 2012
         # divide data by year
-        self.index_train=[]
-        self.index_val=[]
+        self.index_train = []
+        self.index_val = []
         for i in range(self.data_yield.shape[0]):
-            if self.data_yield[i,0]==self.year:
+            if self.data_yield[i, 0] == self.year:
                 self.index_val.append(i)
             else:
                 self.index_train.append(i)
-        self.index_train=np.array(self.index_train)
-        self.index_val=np.array(self.index_val)
+        self.index_train = np.array(self.index_train)
+        self.index_val = np.array(self.index_val)
 
-        
     def create_yield_csv(self):
-        """Build the csv of yields only for the files that exist in output_dir"""
+        """
+        Build the csv of yields only for the files that exist in output_dir.
+        """
         files = [f for f in listdir(self.dir) if isfile(join(self.dir, f))]  # output files created in preprocess_data
-        print ("number of files: {}".format(len(files)))
+        print("number of files: {}".format(len(files)))
         with open(self.data_yield_subset_file, 'wb') as yield_csv:
             for f in files:
                 # for each file, if there is a corresponding yield, write to a new subset file
@@ -113,8 +109,9 @@ class CropHistograms():
                         wr = csv.writer(yield_csv)
                         wr.writerow(yield_str)
 
-    def filter_timespan(self,image_temp,start_day,end_day,bands):
-        """Filter the current image by a given time frame
+    def filter_timespan(self, image_temp, start_day, end_day, bands):
+        """
+        Filter the current image by a given time frame.
 
         Parameters
         ----------
@@ -132,15 +129,15 @@ class CropHistograms():
         numpy array
             the filtered image
         """
-        start_index=int(math.floor(start_day/8))*bands
-        end_index=int(math.floor(end_day/8))*bands
-        if end_index>image_temp.shape[2]:
-            image_temp = np.concatenate((image_temp, 
-                np.zeros((image_temp.shape[0],image_temp.shape[1],end_index-image_temp.shape[2]))),axis=2)
-        return image_temp[:,:,start_index:end_index]
+        start_index = int(math.floor(start_day / 8)) * bands
+        end_index = int(math.floor(end_day / 8)) * bands
+        if end_index > image_temp.shape[2]:
+            image_temp = np.concatenate(
+                (image_temp, np.zeros((image_temp.shape[0], image_temp.shape[1], end_index - image_temp.shape[2]))),
+                axis=2)
+        return image_temp[:, :, start_index:end_index]
 
-
-    def calc_histogram(self,image_temp,bin_seq,bins,times,bands):
+    def calc_histogram(self, image_temp, bin_seq, bins, times, bands):
         """
 
         Parameters
@@ -161,7 +158,7 @@ class CropHistograms():
         hist
             numpy histogram object
         """
-        hist=np.zeros([bins,times,bands])  # build zeroed numpy array of shape (bins, times, bands)
+        hist = np.zeros([bins, times, bands])  # build zeroed numpy array of shape (bins, times, bands)
         for i in range(image_temp.shape[2]):
             # get the densities for the provided sequence and fill the histogram with the mean density
             density, _ = np.histogram(image_temp[:, :, i], bin_seq, density=False)
@@ -170,12 +167,14 @@ class CropHistograms():
 
     # save supervised data
     def build_and_save_histogram(self):
-        """Build and save the histogram based on the file corresponding to the provided yield."""
+        """
+        Build and save the histogram based on the file corresponding to the provided yield.
+        """
         output_image = np.zeros([self.index_all.shape[0], 32, 32, 9])
         output_yield = np.zeros([self.index_all.shape[0]])
         output_year = np.zeros([self.index_all.shape[0]])
-        output_locations = np.zeros([self.index_all.shape[0],2])
-        output_index = np.zeros([self.index_all.shape[0],2])
+        output_locations = np.zeros([self.index_all.shape[0], 2])
+        output_index = np.zeros([self.index_all.shape[0], 2])
 
         for i in self.index_all:
             year = str(int(self.data_yield[i, 0]))
@@ -188,14 +187,14 @@ class CropHistograms():
                     location = l
                     break
 
-            key = np.array([int(loc1),int(loc2)])
+            key = np.array([int(loc1), int(loc2)])
             longitude = location[2]
             latitude = location[3]
 
             filename = year + '_' + loc1 + '_' + loc2 + '.npy'
             image_temp = np.load(self.dir + filename)
             # Paper uses 281 for end_day, code uses 305 for end_day in the following line:
-            image_temp = self.filter_timespan(image_temp, 49, 305, 9)
+            image_temp = self.filter_timespan(image_temp=image_temp, start_day=49, end_day=305, bands=9)
             image_temp[np.isnan(image_temp)] = 0
 
             # 33 objects, evenly spaced between 1 and 4999 - approximately 151 step size between
@@ -204,7 +203,7 @@ class CropHistograms():
             bin_seq = np.linspace(1, 4999, 33)
             # calc_histogram inputs: img, bin_seq, bins, times, bands
             # in the paper, times = 30, here times = 32 possibly due to using 305 days vs 281 in the paper
-            image_temp = self.calc_histogram(image_temp, bin_seq ,32, 32, 9)
+            image_temp = self.calc_histogram(image_temp=image_temp, bin_seq=bin_seq, bins=32, times=32, bands=9)
             image_temp[np.isnan(image_temp)] = 0
 
             # i is matched with data_yield at beginning of for loop
@@ -213,21 +212,19 @@ class CropHistograms():
             output_year[i] = int(year)
             output_locations[i, 0] = longitude
             output_locations[i, 1] = latitude
-            output_index[i,:] = np.array([int(loc1),int(loc2)])
+            output_index[i, :] = np.array([int(loc1), int(loc2)])
 
-            print i,np.sum(image_temp),year,loc1,loc2
-        np.savez(self.dir+'histogram_all_full.npz',
-                 output_image=output_image,output_yield=output_yield,
-                 output_year=output_year,output_locations=output_locations,output_index=output_index)
-        print 'save done'
+            print(i, np.sum(image_temp), year, loc1, loc2)
+        np.savez(self.dir + 'histogram_all_full.npz', output_image=output_image, output_yield=output_yield,
+                 output_year=output_year, output_locations=output_locations, output_index=output_index)
+        print('save done')
 
-        
+
 if __name__ == '__main__':
-
-    output_dir='/content/ee-data/img_full_output/'
+    output_dir = '/content/ee-data/img_full_output/'
     data_yield_file = '/content/datalab/notebooks/crop_yield_prediction/2 clean data/yield_final.csv'
     data_yield_subset_file = '/content/datalab/notebooks/crop_yield_prediction/2 clean data/yield_final_subset.csv'
-    locations_file = '/content/datalab/notebooks/crop_yield_prediction/2 clean data/locations_final.csv'        
+    locations_file = '/content/datalab/notebooks/crop_yield_prediction/2 clean data/locations_final.csv'
 
-    data=CropHistograms(output_dir, data_yield_file, data_yield_subset_file, locations_file)
+    data = CropHistograms(output_dir, data_yield_file, data_yield_subset_file, locations_file)
     data.build_and_save_histogram()
