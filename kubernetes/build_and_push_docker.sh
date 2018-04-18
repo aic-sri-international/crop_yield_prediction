0c5e4730cf10b6806b0652a8ba6c105232a3c82b
Leah Thompson <leah.thompson@sri.com>
2018-04-18 11:35:19 -0600

    update kubernetes and docker files

--- kubernetes/build_and_push_docker.sh
+++ kubernetes/build_and_push_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build . -t worldmodelers.cse.sri.com/crop-yield:v1.2
-docker push worldmodelers.cse.sri.com/crop-yield:v1.2
+docker build . -t worldmodelers.cse.sri.com/crop-yield:v1.3
+docker push worldmodelers.cse.sri.com/crop-yield:v1.3
